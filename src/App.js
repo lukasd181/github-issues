66f086ba6f78dae5037f42590826d4dc1ca65cb4
Lukas <hungduonggia181@gmail.com>
2020-09-24 15:06:45 +0700

    latest page set up

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import SearchBox from "./components/searchBox";
 import { css } from "@emotion/core";
 import ClipLoader from "react-spinners/ClipLoader";
+import Modal from "./components/modal";
+import Pagination from "./components/pagination";
+import IssuesList from "./components/issuesList";
 
 const override = css`
   display: block;
@@ -18,13 +21,6 @@ function App() {
   let [owner, setOwner] = useState("");
   let [loading, setLoading] = useState(null);
 
-  //Goal: get search keyword and show data
-  //1. get the owner and repo from keyowrd
-  //2. call api
-  //2.1 if there is no repo or owner, OR for some reason, api not work
-  //then throw the error
-  //3. show data
-  //before it show result, it will show loading sign
   const handleSubmit = () => {
     console.log("keyword", keyword);
     let { owner, repo } = getOwnerRepo(keyword);
@@ -32,8 +28,6 @@ function App() {
       setError("Error");
       return;
     }
-    console.log(owner);
-    console.log(repo);
     setRepo(repo);
     setOwner(owner);
     setError(null);
@@ -47,9 +41,8 @@ function App() {
   const getIssues = async () => {
     try {
       setLoading(true);
-      const url = `https://api.github.com//repos/${owner}/${repo}/issues`;
+      const url = `https://api.github.com/repos/${owner}/${repo}/issues`;
       const response = await fetch(url);
-      console.log("Im here");
       if (response.status == 200) {
         const data = await response.json();
         console.log("data", data);
@@ -71,21 +64,20 @@ function App() {
 
   return (
     <div>
-      <h1>{keyword}</h1>
+     
       <SearchBox setKeyword={setKeyword} handleSubmit={handleSubmit} />
       {error && (
         <Alert key={0} variant="danger">
           {error}
         </Alert>
       )}
-      <div className="sweet-loading">
-        <ClipLoader
-          css={override}
-          size={150}
-          color={"#123abc"}
-          loading={loading}
-        />
-      </div>
+
+      <ClipLoader
+        css={override}
+        size={150}
+        color={"#123abc"}
+        loading={loading}
+      />
 
       <button type="button">Show Modal</button>
     </div>
---
+++ src/components/issuesList/index.js
@@ -0,0 +1,12 @@
+import styles from "./issuesList.module.css";
+import React from 'react'
+
+const IssuesList = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default IssuesList
